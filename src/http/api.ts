754a17ba62df6397e86e0e8764d3e9ef7f5ec56e
thakur0607 <shivam.thakur@livcheers.com>
2025-08-05 22:41:40 +0530

    Auto Token Refresh on Page Reload

--- src/http/api.ts
+++ src/http/api.ts
@@ -3,7 +3,5 @@ import { api } from "./client";
 
 export const login = (credentials: Credentials) =>
   api.post("/auth/login", credentials);
-
 export const self = () => api.get("/auth/me");
-
 export const logout = () => api.post("/auth/logout");
